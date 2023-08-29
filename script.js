cf4dce5fd3fb65152c2a90fb2ca206c2683df606
downlifted <38848227+downlifted@users.noreply.github.com>
2023-08-29 09:22:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDPRFnBR6P2q__VRbWDb4Q1pCz1_LqhnyE",
+  authDomain: "alex-tribute.firebaseapp.com",
+  projectId: "alex-tribute",
+  storageBucket: "alex-tribute.appspot.com",
+  messagingSenderId: "631521944474",
+  appId: "1:631521944474:web:f1cdbdb75a4bc7ea049023",
+  measurementId: "G-PZ5FZXEV1R"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();

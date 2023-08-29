711c9dcec6d2e788af9428712f4edf41fc51834c
downlifted <38848227+downlifted@users.noreply.github.com>
2023-08-29 09:25:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,48 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+const memoryForm = document.getElementById('memory-form');
+const memoryContainer = document.getElementById('memories-list');
+
+memoryForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const memory = document.getElementById('memory').value;
+
+    if (!name || !memory) {
+        return;
+    }
+
+    // Store memory in Firebase
+    const memoryData = {
+        name,
+        memory,
+        timestamp: new Date().toISOString(),
+    };
+
+    database.ref('memories').push(memoryData);
+
+    // Clear the form fields after submission
+    document.getElementById('name').value = '';
+    document.getElementById('memory').value = '';
+});
+
+// Fetch and display memories
+const memoriesRef = database.ref('memories');
+memoriesRef.on('value', snapshot => {
+    memoryContainer.innerHTML = ''; // Clear existing memories
+    snapshot.forEach(memorySnapshot => {
+        const memory = memorySnapshot.val();
+        const memoryItem = document.createElement('div');
+        memoryItem.classList.add('memory-item');
+        memoryItem.innerHTML = `
+            <p><strong>${memory.name}</strong> said:</p>
+            <p>${memory.memory}</p>
+        `;
+        memoryContainer.appendChild(memoryItem);
+    });
+});

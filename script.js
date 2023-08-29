361c52167b554481b31a51f1c0d262cc241301e3
downlifted <38848227+downlifted@users.noreply.github.com>
2023-08-29 09:40:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDPRFnBR6P2q__VRbWDb4Q1pCz1_LqhnyE",
-  authDomain: "alex-tribute.firebaseapp.com",
-  projectId: "alex-tribute",
-  storageBucket: "alex-tribute.appspot.com",
-  messagingSenderId: "631521944474",
-  appId: "1:631521944474:web:f1cdbdb75a4bc7ea049023",
-  measurementId: "G-PZ5FZXEV1R"
-};
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-
+// script.js
 const memoryForm = document.getElementById('memory-form');
 const memoryContainer = document.getElementById('memories-list');
 
@@ -24,32 +11,13 @@ memoryForm.addEventListener('submit', event => {
         return;
     }
 
-    // Store memory in Firebase
-    const memoryData = {
-        name,
-        memory,
-        timestamp: new Date().toISOString(),
-    };
-
-    database.ref('memories').push(memoryData);
+    // Store memory using the function from airtable.js
+    submitMemory(name, memory);
 
     // Clear the form fields after submission
     document.getElementById('name').value = '';
     document.getElementById('memory').value = '';
 });
 
-// Fetch and display memories
-const memoriesRef = database.ref('memories');
-memoriesRef.on('value', snapshot => {
-    memoryContainer.innerHTML = ''; // Clear existing memories
-    snapshot.forEach(memorySnapshot => {
-        const memory = memorySnapshot.val();
-        const memoryItem = document.createElement('div');
-        memoryItem.classList.add('memory-item');
-        memoryItem.innerHTML = `
-            <p><strong>${memory.name}</strong> said:</p>
-            <p>${memory.memory}</p>
-        `;
-        memoryContainer.appendChild(memoryItem);
-    });
-});
+// Fetch and display memories (as before)
+// ...
